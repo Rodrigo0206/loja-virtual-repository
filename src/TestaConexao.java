78a424d3fc0c06356542ba57aa25e2558ef10394
unknown <rodrigo.domingues.87@hotmail.com>
2022-02-10 10:01:34 -0300

    Primeira conexao

---
+++ src/TestaConexao.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestaConexao {
+
+	public static void main(String[] args) throws SQLException {
+	
+		Connection connection = DriverManager
+				.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=UTC", "root", "13027700"); 
+		
+		System.out.println("Fechando cinexao!!");
+		
+		connection.close();
+	}
+
+}

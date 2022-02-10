6dbfb2229a1edaade6d9b079d80c1ebc70e14461
unknown <rodrigo.domingues.87@hotmail.com>
2022-02-10 10:10:24 -0300

    Fechando conexão

--- src/TestaConexao.java
+++ src/TestaConexao.java
@@ -7,9 +7,9 @@ public class TestaConexao {
 	public static void main(String[] args) throws SQLException {
 	
 		Connection connection = DriverManager
-				.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=UTC", "root", "13027700"); 
+				.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "13027700"); 
 		
-		System.out.println("Fechando cinexao!!");
+		System.out.println("Fechando conexao!!");
 		
 		connection.close();
 	}

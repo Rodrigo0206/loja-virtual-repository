865737ebfeaccf4858c9d54175b42ea109dbae8f
unknown <rodrigo.domingues.87@hotmail.com>
2022-02-11 10:30:04 -0300

    Connection Factory

--- src/TestaConexao.java
+++ src/TestaConexao.java
@@ -1,16 +1,14 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class TestaConexao {
 
 	public static void main(String[] args) throws SQLException {
 	
-		Connection connection = DriverManager
-				.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "13027700"); 
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection connection = connectionFactory.recuperarConexao();
 		
-		
-		
+		System.out.println("Fechando conexao");
 		connection.close();
 	}
 
--- src/TestaListagem.java
+++ src/TestaListagem.java
@@ -1,34 +1,36 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class TestaListagem {
-	
+
 	public static void main(String[] args) throws SQLException {
+
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection connection = connectionFactory.recuperarConexao();
+
 		
-		Connection con = DriverManager
-				.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", 
-						"root", "13027700"); 
-		
-		
-		Statement stm = con.createStatement();
+		Statement stm = connection.createStatement();
 		stm.execute("SELECT ID, NOME, DESCRICACAO FROM PRODUTO");
-		
+
 		ResultSet rst = stm.getResultSet();
-		
-		while(rst.next()) {
+
+		while (rst.next()) {
 			Integer id = rst.getInt("ID");
 			System.out.println(id);
 			String nome = rst.getString("NOME");
 			System.out.println(nome);
-			String descricao = rst.getString("DESCRICACAO");	
+			String descricao = rst.getString("DESCRICACAO");
 			System.out.println(descricao);
 		}
-		con.close();
+		connection.close();
 
-		
+	}
+
+	private static ConnectionFactory CriaConexao() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
